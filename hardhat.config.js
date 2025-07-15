3197abc7fb9ff70cb6a2b6cfcb322f19b2e471e9
xumengxia <1540926580@qq.com>
2025-07-15 15:22:57 +0800

    使用 hardhat-deploy 提取公共测试部分

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox");
 // require("dotenv").config();
 require("@chainlink/env-enc").config(); // 加密之后的
 require("./tasks/index");
+require("hardhat-deploy"); // 测试
 
 const SEPOLIA_URL = process.env.SEPOLIA_URL;
 const PRIVATA_KEY = process.env.PRIVATA_KEY; //matemask私钥
@@ -9,7 +10,7 @@ const PRIVATA_KEY_2 = process.env.PRIVATA_KEY_2;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  // defaultNetwork: "hardhat", // sepolia
+  defaultNetwork: "hardhat", // sepolia
   solidity: "0.8.28",
   networks: {
     sepolia: {
@@ -21,6 +22,14 @@ module.exports = {
   etherscan: {
     apiKey: {
       sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  namedAccounts: {
+    firstAccount: {
+      default: 0, // 这里默认用的是第一个账号
+    },
+    secondAccount: {
+      default: 2, // 对应我第二个账号
     },
   },
 };
--- test/fundme.test.js
+++ test/fundme.test.js
@@ -1,17 +1,21 @@
-const { ethers } = require("hardhat");
+const { ethers, deployments } = require("hardhat");
 const { assert } = require("chai");
 
 describe("test fundme contract", async function () {
+  // beforeEach 每个节点运行的时候都会执行一次
+  let fundMe;
+  let firstAccount;
+  beforeEach(async function () {
+    await deployments.fixture(["all"]);
+    firstAccount = (await getNamedAccounts()).firstAccount;
+    const fundMeDeployment = await deployments.get("FundMe");
+    fundMe = await ethers.getContractAt("FundMe", fundMeDeployment.address);
+  });
   it("test if the owner is msg.sender", async function () {
-    const [firstAccount] = await ethers.getSigners();
-    const fundMeFactory = await ethers.getContractFactory("FundMe");
-    const fundMe = await fundMeFactory.deploy(180); // 180 seconds
     await fundMe.waitForDeployment();
-    assert.equal(await fundMe.owner(), firstAccount.address);
+    assert.equal(await fundMe.owner(), firstAccount);
   });
   it("test if the datafeed is assigned correctly", async function () {
-    const fundMeFactory = await ethers.getContractFactory("FundMe");
-    const fundMe = await fundMeFactory.deploy(180); // 180 seconds
     await fundMe.waitForDeployment();
     assert.equal(
       await fundMe.dataFeed(),
